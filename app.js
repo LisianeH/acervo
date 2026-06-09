f8a7c71279c93075dc9bb0b83e535e41c0e16b84
Nicolas Brum <nickormy@gmail.com>
2026-06-09 20:19:11 -0300

    fix: rename repositories imports, produtos to usuarios

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const PORT = 3002;
 
 app.use(express.json());
 
-const produtoRouter = require('./router/produto_router.js');
+const usuarioRouter = require("./router/usuario_router.js");
 
-app.use("/api/produtos", produtoRouter);
+app.use("/api/usuarios", usuarioRouter);
 
 // Tratamento de erro, com rota não encontrada
 app.use((req, res) => {
-    res.status(404).send('Rota não encontrada')
+  res.status(404).send("Rota não encontrada");
 });
 
 // Inicial no terminal
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
--- database/database.js
+++ database/database.js
@@ -1,11 +1,11 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'postgre',
-    database: 'crud_acervo', 
+  host: "localhost",
+  port: 5432,
+  user: "postgres",
+  password: "admin",
+  database: "crud_acervo",
 });
 
 module.exports = pool;
